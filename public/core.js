518a4514fa4dce662977d1f50b39ba1dba458ac5
mollymerp <mollylloyd@gmail.com>
2015-10-22 16:21:26 -0700

    [style] installed and ran HTML prettify

--- public/core.js
+++ public/core.js
@@ -1,11 +1,11 @@
-angular.module("knapsack", ["ui.router","ui.bootstrap","smart-table","knapsack.services","knapsack.main"])
+angular.module("knapsack", ["ui.router", "ui.bootstrap", "smart-table", "knapsack.services", "knapsack.main"])
 
-.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouteProvider) {
+.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouteProvider) {
   $urlRouteProvider.otherwise("/");
 
   $stateProvider
     .state("main", {
-      url:"/",
+      url: "/",
       templateUrl: "main/main.html",
       controller: "MainController"
     })
