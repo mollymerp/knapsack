bf47e3ffde26738d7094f1c9ff7a3dda8b8b7a98
mollymerp <mollylloyd@gmail.com>
2015-10-22 16:06:13 -0700

    [style] reworked some variable/parameter naming

--- public/main/main.js
+++ public/main/main.js
@@ -1,13 +1,18 @@
 angular.module("knapsack.main",[])
 .controller("MainController", ["$scope","Contents", function ($scope, Contents){
-  $scope.booksCollection = [{"title": "The Goldfinch", "author": "Donna Tartt", "readStatus":"Reading now"},
+  $scope.bookCollection = [{"title": "The Goldfinch", "author": "Donna Tartt", "readStatus":"Reading now"},
                             {"title": "Harry Potter", "author": "J.K. Rowling", "readStatus": "Finished"},
-                            {"title": "Just Kids", "author": "Patti Smith", "readStatus": "To read"}];
+                            {"title": "Just Kids", "author": "Patti Smith", "readStatus": "To read"},
+                            {"title": "Pro AngularJS", "author":"Adam Freeman", "readStatus": "Never"}];
 
-  // $scope.getBooks = function (){
-  //   Contents.getContent("books").then(function (resp){
-  //     return;
-  //   });
-  // };
+  $scope.articleCollection = [];
+
+  $scope.getBooks = function (){
+
+  };
+
+  $scope.removeBook = function (book) {
+
+  };
 
 }]);
