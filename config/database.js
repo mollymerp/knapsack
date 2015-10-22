05ef64419395874a3a1431eb240f774c95f7943d
Chris Puskar <cjpuskar@comcast.net>
2015-10-22 15:08:26 -0700

    [feat]: Addinfo for database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,20 @@
-/*
-Placeholder for database config file.
+var mysql = require('mysql');
+​
+var pool = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'ks_db'
+});
+​
+//Why use pool to connect to mysql:
+//http://stackoverflow.com/questions/26432178/what-is-the-difference-between-mysql-createconnection-and-mysql-createpool-in-no
+​
+pool.connect=function(callback){
+  pool.getConnection(function(err, connection){
+    callback(connection);
+  });
+};
+​
+module.exports = pool;
 
-
-*/
