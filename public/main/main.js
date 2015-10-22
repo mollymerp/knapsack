0c4fb4b9f6075c13499e3c0ad4e881ef27e8deda
mollymerp <mollylloyd@gmail.com>
2015-10-22 16:21:26 -0700

    [style] installed and ran HTML prettify

--- public/main/main.js
+++ public/main/main.js
@@ -1,18 +1,31 @@
-angular.module("knapsack.main",[])
-.controller("MainController", ["$scope","Contents", function ($scope, Contents){
-  $scope.bookCollection = [{"title": "The Goldfinch", "author": "Donna Tartt", "readStatus":"Reading now"},
-                            {"title": "Harry Potter", "author": "J.K. Rowling", "readStatus": "Finished"},
-                            {"title": "Just Kids", "author": "Patti Smith", "readStatus": "To read"},
-                            {"title": "Pro AngularJS", "author":"Adam Freeman", "readStatus": "Never"}];
+angular.module("knapsack.main", [])
+  .controller("MainController", ["$scope", "Contents", function($scope, Contents) {
+    $scope.bookCollection = [{
+      "title": "The Goldfinch",
+      "author": "Donna Tartt",
+      "readStatus": "Reading now"
+    }, {
+      "title": "Harry Potter",
+      "author": "J.K. Rowling",
+      "readStatus": "Finished"
+    }, {
+      "title": "Just Kids",
+      "author": "Patti Smith",
+      "readStatus": "To read"
+    }, {
+      "title": "Pro AngularJS",
+      "author": "Adam Freeman",
+      "readStatus": "Never"
+    }];
 
-  $scope.articleCollection = [];
+    $scope.articleCollection = [];
 
-  $scope.getBooks = function (){
+    $scope.getBooks = function() {
 
-  };
+    };
 
-  $scope.removeBook = function (book) {
+    $scope.removeBook = function(book) {
 
-  };
+    };
 
-}]);
+  }]);
