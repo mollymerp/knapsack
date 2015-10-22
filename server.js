3b5e95db3af04b1ce10f8f73627959d596783d60
Chris Puskar <cjpuskar@comcast.net>
2015-10-22 15:02:17 -0700

    [chore]: Addproject files to my forked local repo

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');   
+var bodyParser = require('body-parser'); // pull reqs from HTML POST
+var morgan = require('morgan');  // log requests to the console
+var db = require('./config/database'); 
+
+
+
+var app = express();  // create our app w/ express
+var port = process.env.PORT || 3000;
+var ip = "127.0.0.1";
+
+/************************************************************/
+// CONFIGURE SERVER
+/************************************************************/
+//placeholder for connecting to DB
+db.connect()
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+
+
+/************************************************************/
+// ROUTE HANDLING
+/************************************************************/
+app.get('/', function(req, res) {
+  res.send('Hello');
+});
+
+
+/************************************************************/
+// Authenticaton Routes
+/************************************************************/
+
+
+
+
+/************************************************************/
+// Handle the wildcard route last - if all other routes fail
+// 
+// 
+/************************************************************/
+
+console.log('Knapsack is listening on port ' + port);
+app.listen(3000);
