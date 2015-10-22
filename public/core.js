2b6ac5d7bded909fb6106dcb9817da272ced8bd8
mollymerp <mollylloyd@gmail.com>
2015-10-22 15:47:57 -0700

    [feat]: accordion table working (for now)

---
+++ public/core.js
@@ -0,0 +1,18 @@
+angular.module("knapsack", ["ui.router","ui.bootstrap","smart-table","knapsack.services","knapsack.main"])
+
+.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouteProvider) {
+  $urlRouteProvider.otherwise("/");
+
+  $stateProvider
+    .state("main", {
+      url:"/",
+      templateUrl: "main/main.html",
+      controller: "MainController"
+    })
+
+}]);
+// .run(["$rootScope", "$location", "Auth", function ($rootScope, $location, Auth){
+//   $rootScope.on("$routeChangeStart", function (){
+//     $location.path("/");
+//   })
+// }])
