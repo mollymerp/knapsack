c58d7aee0eb9d8289e2db91983dff96f2edbc9c5
arthur <arthur.mathies@googlemail.com>
2015-10-23 07:21:41 -0700

    [feat]: add sidebar http requests
    - addCollection and removeCollection

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,50 +1,76 @@
 angular.module("knapsack.services", [])
   .factory("Collections", ["$http", function($http) {
+
     var getAll = function() {
+      return $http({
+        method: "GET",
+        url: "api/collections"
+      }).then(function succesCallback(resp) {
+        console.log(resp.status + ": succesfully fetched collections");
+      }, function errorCallback(resp) {
+        console.log(resp.status + ": failed fetching from server");
+      })
       //will be an http request at some point but not for now just return somehting
       return ["wine", "football", "cars", "trees", "boats"];
     };
 
-    return {
-      getAll: getAll
-    };
-  }])
-.factory("Contents", ["$http", function($http) {
-
-  var getContent = function() {
-    return $http({
-        method: "GET",
-        url: "/"
-      })
-      .then(function(resp) {
-        return resp.data;
-      });
-  };
-
-  var addContent = function(content) {
-    return $http({
+    var addCollection = function() {
+      return $http({
         method: "POST",
-        url: "/"
+        url: "api/collections"
+      }).then(function succesCallback(resp) {
+        console.log(resp.status + ": succesfully added Collection");
+      }, function errorCallback(resp) {
+        console.log(resp.status + ": failed adding Collection");
       })
-      .then(function(resp) {
-        console.log("data saved")
-      });
-  };
+    };
 
-  var removeContent = function(content) {
-    return $http({
-        method: 'POST',
-        url: "/"
-      })
-      .then(function(resp) {
+    var removeCollection = function() {
+      return $http({});
+    }
+    return {
+      getAll: getAll,
+      addCollection: addCollection,
+      removeCollection: removeCollection
+    };
 
-      });
-  }
+  }])
+  .factory("Contents", ["$http", function($http) {
 
-  return {
-    getContent: getContent,
-    addContent: addContent,
-    removeContent: removeContent
-  };
+    var getContent = function() {
+      return $http({
+          method: "GET",
+          url: "/"
+        })
+        .then(function(resp) {
+          return resp.data;
+        });
+    };
 
-}])
+    var addContent = function(content) {
+      return $http({
+          method: "POST",
+          url: "/"
+        })
+        .then(function(resp) {
+          console.log("data saved")
+        });
+    };
+
+    var removeContent = function(content) {
+      return $http({
+          method: 'POST',
+          url: "/"
+        })
+        .then(function(resp) {
+
+        });
+    }
+
+    return {
+      getContent: getContent,
+      addContent: addContent,
+      removeContent: removeContent
+    };
+
+  }])
