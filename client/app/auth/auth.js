44935c7f386eec62d4809a5bb1568f77e08d21d4
arthur <arthur.mathies@googlemail.com>
2015-10-23 12:02:31 -0700

    [refactor]: use bracket angular notation for params

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,35 +1,35 @@
 angular.module("knapsack.auth", ['ui.bootstrap'])
 
-.controller('authController', function($scope, $uibModal, $log){
+.controller('authController', ["$scope", "$uibModal", "$log", function($scope, $uibModal, $log) {
 
-  $scope.open = function (size) {
+  $scope.open = function(size) {
 
     var modalInstance = $uibModal.open({
       templateUrl: 'app/auth/login-modal.html',
       controller: ModalInstanceCtrl,
       size: size,
       resolve: {
-                    userForm: function () {
-                        return $scope.userForm;
-                    }
-                }
+        userForm: function() {
+          return $scope.userForm;
+        }
+      }
     });
   };
 
-});
+}]);
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, userForm) {
-    $scope.form = {}
-    $scope.submitForm = function () {
-        if ($scope.form.userForm.$valid) {
-            console.log('user form is in scope');
-            $modalInstance.close('closed');
-        } else {
-            console.log('userform is not in scope');
-        }
-    };
+var ModalInstanceCtrl = function($scope, $modalInstance, userForm) {
+  $scope.form = {}
+  $scope.submitForm = function() {
+    if ($scope.form.userForm.$valid) {
+      console.log('user form is in scope');
+      $modalInstance.close('closed');
+    } else {
+      console.log('userform is not in scope');
+    }
+  };
 
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
 };
