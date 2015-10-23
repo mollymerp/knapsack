c57f3f3d0f629be4c10b8a4ea51fc1c8a1de8cc1
mollymerp <mollylloyd@gmail.com>
2015-10-22 18:41:54 -0700

    [refactor]: prepare for merge with molly's feature

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,25 @@
-angular.module("knapsack", ["knapsack.services",
-    "knapsack.sidebar",
-    "ngRoute"
-  ])
-  .config(["$routeProvider", "$httpProvider", function($routeProvider, $httpProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "sidebar.html",
-        controller: "SidebarController",
-        controllerAs: "sidebar"
-      })
-      .otherwise({
-        redirectTo: "/"
-      });
-  }]);
+angular.module("knapsack", ["ui.router", "ui.bootstrap", "smart-table", "knapsack.services", "knapsack.main"])
+
+.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouteProvider) {
+  $urlRouteProvider.otherwise("/");
+
+  $stateProvider
+    .state("main", {
+      url: "/"
+      views: {
+        "main_lists": {
+          templateUrl: "app/main_lists/main.html",
+          controller: "MainController"
+        },
+        "sidebar": {
+          templateUrl: "sidebar.html",
+          controller: "SidebarController",
+          controllerAs: "sidebar"
+        },
+        "header": {
+          
+        }
+      }
+    })
+
+}]);
