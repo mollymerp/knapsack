96cb05e7a4d4fe06d3a3dabeebe004eb22433326
Chris Puskar <cjpuskar@comcast.net>
2015-10-23 14:55:44 -0700

    [style]: Change single quotes to double quotes to conform to group style guide

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser'); // pull reqs from HTML POST
-var morgan = require('morgan');  // log requests to the console
-var db = require('../config/database'); 
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+var express = require("express");
+var bodyParser = require("body-parser"); // pull reqs from HTML POST
+var morgan = require("morgan");  // log requests to the console
+var db = require("../config/database"); 
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
 
 
 
@@ -22,23 +22,23 @@ var ip = "127.0.0.1";
 
 
 // Express uses template engine to parse front-end scripts. Can parse HTML, EJS, JADE, ect
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Tells Express from where to deliver front end views
-app.set('views', __dirname + '/../client/views')
+app.set("views", __dirname + "/../client/views")
 // Logger for dev environment
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Body parser is middleware to handle POST data in Express 4
-app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.urlencoded({"extended":"true"}));
 app.use(bodyParser.json());
 // Cookie parser is middleware to handle cookies.
 app.use(cookieParser());
 // Express sessions handles sessions in Express
-app.use(session({secret: '$#%!@#@@#SSDASASDVV@@@@', 
-                 key: 'sid',
+app.use(session({secret: "$#%!@#@@#SSDASASDVV@@@@", 
+                 key: "sid",
                  saveUninitialized: true,
                  resave: true}));
 // serve up static files
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + "/../client"));
 
 
 /************************************************************/
@@ -49,27 +49,27 @@ app.use(express.static(__dirname + '/../client'));
 var router  = express.Router();
 
 // Home page route (http://localhost:3000)
-router.get('/', function(req, res) {
+router.get("/", function(req, res) {
   res.render("index");
 });
 
-app.post('/', function(req, res) {
+app.post("/", function(req, res) {
   res.send("I got a POST Request from the home page");
 });
 
 // 
-// app.post('/login', function(req, res) {
+// app.post("/login", function(req, res) {
 //   var name = req.body.name;
 //   var username = req.body.username;
 //   var email = req.body.email;
 //   console.log("Name: ", name, "Username: ", username, "Email: ", email);
 // });
 
-// app.post('/signin', function(req, res) {
+// app.post("/signin", function(req, res) {
 //   console.log(req);
 // });
 // apply the routes to our application
-app.use('/', router);
+app.use("/", router);
 
 app.post("/api/signin", function(req, res) {
   console.log(req.body);
@@ -101,4 +101,4 @@ app.post("/api/signup", function(req, res) {
 // START THE SERVER
 /************************************************************/
 app.listen(port);
-console.log('Knapsack is listening on port ' + port);
+console.log("Knapsack is listening on port " + port);
