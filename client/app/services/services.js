ad218b1c08b51e479dc54a75ce26f8747cae2753
arthur <arthur.mathies@googlemail.com>
2015-10-23 07:43:46 -0700

    [refactor]: modify  requests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -7,27 +7,44 @@ angular.module("knapsack.services", [])
         url: "api/collections"
       }).then(function succesCallback(resp) {
         console.log(resp.status + ": succesfully fetched collections");
+        return resp.data;
       }, function errorCallback(resp) {
         console.log(resp.status + ": failed fetching from server");
-      })
+      });
       //will be an http request at some point but not for now just return somehting
       return ["wine", "football", "cars", "trees", "boats"];
     };
 
-    var addCollection = function() {
+    var addCollection = function(data) {
       return $http({
         method: "POST",
-        url: "api/collections"
+        url: "api/collections",
+        data: data
       }).then(function succesCallback(resp) {
         console.log(resp.status + ": succesfully added Collection");
       }, function errorCallback(resp) {
         console.log(resp.status + ": failed adding Collection");
-      })
+      });
     };
 
-    var removeCollection = function() {
-      return $http({});
-    }
+    var removeCollection = function(colName) {
+      return $http({
+        method: "DELETE",
+        url: "api/collections",
+        data: colName
+          //maybe we also need to send over the user if 
+          //the server cannot identify us from the session cookie
+      }).then(function succesCallback(resp) {
+        console.log(resp.status + ": succesfully deleted Collection");
+      }, function errorCallback(resp) {
+        console.log(resp.status + ": failed deleting Collection");
+      });
+    };
+    
+    //maybe use angular interceptors to make success and error handling nicer
+    //and also maybe parsing the response that is coming as json if needed 
+    //https://docs.angularjs.org/api/ng/service/$http
+
     return {
       getAll: getAll,
       addCollection: addCollection,
