c2028d2496125afe93980cbf3ade2062f8ebadb5
mollymerp <mollylloyd@gmail.com>
2015-10-22 17:31:30 -0700

    [style]: updated file structure

---
+++ client/app.js
@@ -0,0 +1,26 @@
+angular.module("knapsack", ["ui.router", "ui.bootstrap", "smart-table", "knapsack.services", "knapsack.main","knapsack.sidebar"])
+
+.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouteProvider) {
+  $urlRouteProvider.otherwise("/");
+
+  $stateProvider
+    .state("main",
+      url: "/", { 
+      views: {
+        "main": {
+          templateUrl: "app/main_lists/main.html",
+          controller: "MainController"
+        },
+        "sidebar": {
+          templateUrl: "app/sidebar/sidebar.html",
+          controller: "SidebarController"
+        }
+      }
+    })
+
+}]);
+// .run(["$rootScope", "$location", "Auth", function ($rootScope, $location, Auth){
+//   $rootScope.on("$routeChangeStart", function (){
+//     $location.path("/");
+//   })
+// }])
