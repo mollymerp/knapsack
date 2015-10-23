c7bbf264998c13db1c8533ddd5d343821e2a2938
arthur <arthur.mathies@googlemail.com>
2015-10-23 14:48:48 -0700

    [refactor]: minor modifications to Signup Schema

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,13 +1,14 @@
-angular.module("knapsack.auth", ['ui.bootstrap'])
+angular.module("knapsack.auth", ["ui.bootstrap"])
 
-.controller('authController', ["$scope", "$uibModal", "$log", function($scope, $uibModal, $log) {
+.controller("authController", ["$scope", "$uibModal", "$log", function($scope, $uibModal, $log) {
 
-  $scope.open = function(size) {
+  $scope.open = function() {
 
     var modalInstance = $uibModal.open({
       templateUrl: 'app/auth/login-modal.html',
       controller: ModalInstanceCtrl,
-      size: size,
+      size: "modal-xs",
+      scope: $scope,
       resolve: {
         userForm: function() {
           return $scope.userForm;
@@ -17,6 +18,28 @@ angular.module("knapsack.auth", ['ui.bootstrap'])
   };
 
 }]);
+
+// angular.module("knapsack.auth", ["ui.bootstrap"])
+
+// .controller("ModalInstanceCtrl", function($http, $scope, $modalInstance, userForm) {
+//   $scope.form = {};
+//   $scope.submitForm = function() {
+//     if ($scope.form.userForm.$valid) {
+//       $http({
+//         method: "POST",
+//         url: "api/signup",
+//         data: $scope.user
+//       });
+//       $modalInstance.close('closed');
+//     } else {
+//       console.log("form not valid");
+//     }
+//   };
+
+//   $scope.cancel = function() {
+//     $modalInstance.dismiss('cancel');
+//   };
+// });
 
 var ModalInstanceCtrl = function($http, $scope, $modalInstance, userForm) {
   $scope.form = {};
