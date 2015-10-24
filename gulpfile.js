f080cde5823cb5fa9e570748ce49f72460a16516
ccnixon <chrisnxn@gmail.com>
2015-10-23 17:22:48 -0700

    added a really basic minify as well as added some dev dependencies to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
+var gulp = require("gulp");
+var plugins = require("gulp-load-plugins")();
+var del = require("del");
+var es = require("event-stream");    
+var bowerFiles = require("main-bower-files");
+var print = require("gulp-print");
+var Q = require("q");
+var uglify = require("gulp-uglify")
 
-gulp.task('default', ['watch']);
+var paths = {
+  scripts: ["./client/app/app.js", "./client/app/**/*.js"],
+  styles: "./client/styles/*.css",
+  index: "./client/index.html",
+  partials: ["./client/app/**/*.html"],
+  distDev: "./dist-dev",
+  distProd: "./dist-prod",
+  distScriptsProd: "./dist-prod/scripts"
+};
 
-gulp.task('jshint', function() {
-  return gulp.src('client/app/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+var pipes = {};
+
+gulp.task('default', function() {
+  console.log("Hello World");
 });
 
-gulp.task('watch', function() {
-  gulp.watch('client/app/**/*.js', ['jshint']);
+gulp.task('minify', function () {
+   gulp.src(paths.scripts)
+      .pipe(uglify())
+      .pipe(gulp.dest(paths.distProd))
 });
-
