5b0aeea99a3bafca7f785a4b9833a16ddd17748f
ccnixon <chrisnxn@gmail.com>
2015-10-26 14:11:49 -0700

    made some small changes to gulpfile and removed some unneeded dev dependencies from package json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")();
 var del = require("del");
-var es = require("event-stream");    
 var bowerFiles = require("main-bower-files");
-var print = require("gulp-print");
-var Q = require("q");
 var uglify = require("gulp-uglify")
 
 var paths = {
