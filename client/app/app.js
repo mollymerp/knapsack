c36b291f520d2ae5e8d65438c4d0ab6359f612ea
mollymerp <mollylloyd@gmail.com>
2015-10-27 15:56:15 -0700

    [refactor]: refactor authentication calls with the modal to services.js file, change some styles on modal and landing page

--- client/app/app.js
+++ client/app/app.js
@@ -61,4 +61,28 @@ angular.module("knapsack", [
       }
     })
 
-}]);
+}])
+// .run(['$rootScope', '$urlRouter', '$location', '$state', function ($rootScope, $urlRouter, $location, $state) {
+//     $rootScope.$on('$locationChangeSuccess', function(e, newUrl, oldUrl) {
+//       // Prevent $urlRouter's default handler from firing
+//       e.preventDefault();
+
+//       * 
+//        * provide conditions on when to 
+//        * sync change in $location.path() with state reload.
+//        * I use $location and $state as examples, but
+//        * You can do any logic
+//        * before syncing OR stop syncing all together.
+       
+
+//       if ($state.current.name !== 'landing' ) {
+//         // your stuff
+
+//         $urlRouter.sync();
+//       } else {
+//         // don't sync
+//       }
+//     });
+//     // Configures $urlRouter's listener *after* your custom listener
+//     $urlRouter.listen();
+//   }]);;
