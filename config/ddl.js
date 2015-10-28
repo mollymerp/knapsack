651c4ccb8a55a6c0b6dd296f7d3e32cecc2ccbd4
ccnixon <chrisnxn@gmail.com>
2015-10-27 17:58:15 -0700

    trying to run heroku without db code

--- config/ddl.js
+++ config/ddl.js
@@ -1,60 +1,60 @@
-//build tables
+// //build tables
 
-var db = require("./database.js");
-var Sequelize = require("Sequelize");
+// var db = require("./database.js");
+// var Sequelize = require("Sequelize");
 
-var ddl = {};
+// var ddl = {};
 
-ddl.users = db.define("users", {
+// ddl.users = db.define("users", {
 
-  user_name: {
-    type: Sequelize.STRING,
-    description: Sequelize.TEXT
-  },
+//   user_name: {
+//     type: Sequelize.STRING,
+//     description: Sequelize.TEXT
+//   },
 
-  password: {
-    type: Sequelize.STRING,
-    description: Sequelize.TEXT
-  },
+//   password: {
+//     type: Sequelize.STRING,
+//     description: Sequelize.TEXT
+//   },
 
-  freezeTableName: true
-  //prevents sequelize from adding (s) to end of table
-});
-
-
-ddl.collections = db.define("collections", {
-
-  collection: {
-    type: Sequelize.STRING,
-    description: Sequelize.TEXT
-  },
-
-  freezeTableName: true
-
-});
-
-ddl.books = db.define("books", {
-
-  title: {
-    type: Sequelize.STRING,
-    description: Sequelize.TEXT
-  },
-
-  author: {
-    type: Sequelize.STRING,
-    description: Sequelize.TEXT
-  },
-
-  freezeTableName: true
-
-});
+//   freezeTableName: true
+//   //prevents sequelize from adding (s) to end of table
+// });
 
 
+// ddl.collections = db.define("collections", {
 
-///Set Up Relationships
-ddl.users.hasMany(ddl.collections, {as: 'collection'});
-ddl.collections.belongsToMany(ddl.books, {through : 'collections_to_books'});
-ddl.books.belongsToMany(ddl.collections, {through : 'collections_to_books'});
+//   collection: {
+//     type: Sequelize.STRING,
+//     description: Sequelize.TEXT
+//   },
 
-module.exports = ddl;
+//   freezeTableName: true
+
+// });
+
+// ddl.books = db.define("books", {
+
+//   title: {
+//     type: Sequelize.STRING,
+//     description: Sequelize.TEXT
+//   },
+
+//   author: {
+//     type: Sequelize.STRING,
+//     description: Sequelize.TEXT
+//   },
+
+//   freezeTableName: true
+
+// });
+
+
+
+// ///Set Up Relationships
+// ddl.users.hasMany(ddl.collections, {as: 'collection'});
+// ddl.collections.belongsToMany(ddl.books, {through : 'collections_to_books'});
+// ddl.books.belongsToMany(ddl.collections, {through : 'collections_to_books'});
+
+// module.exports = ddl;
 
