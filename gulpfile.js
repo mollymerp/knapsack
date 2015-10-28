8103ad9001f722daf153fd58a02a1ef2bc59a37e
ccnixon <chrisnxn@gmail.com>
2015-10-28 09:23:30 -0700

    added dependcy injectction into index html and gulp build command for dev environment

--- gulpfile.js
+++ gulpfile.js
@@ -17,14 +17,93 @@ var paths = {
   scripts: ["./client/app/app.js", "./client/app/**/*.js"],
   styles: "./client/styles/*.css",
   index: "./client/index.html",
-  partials: ["./client/app/**/*.html"]
+  partials: ["./client/app/**/*.html"],
+  distDev: './dist.dev'
 };
 
 var pipes = {};
 
+
+//use angular-file-sort to build order of dependencies
 pipes.orderedAppScripts = function(){
   return plugins.angularFilesort();
-}
+};
+
+
+pipes.orderedVendorScripts = function() {
+    return plugins.order(['jquery.js', 'angular.js']);
+};
+
+pipes.validatedAppScripts = function() {
+    return gulp.src(paths.scripts)  
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'));
+};
+
+pipes.builtAppScriptsDev = function() {
+    return pipes.validatedAppScripts()
+        .pipe(gulp.dest(paths.distDev));
+};
+
+pipes.builtVendorScriptsDev = function() {
+    return gulp.src(bowerFiles())
+        .pipe(gulp.dest('dist.dev/bower_components'));
+};
+
+pipes.validatedPartials = function() {
+    return gulp.src(paths.partials)
+        .pipe(plugins.htmlhint({'doctype-first': false}))
+        .pipe(plugins.htmlhint.reporter());
+};
+
+pipes.builtPartialsDev = function() {
+    return pipes.validatedPartials()
+        .pipe(gulp.dest(paths.distDev));
+};
+
+pipes.scriptedPartials = function() {
+    return pipes.validatedPartials()
+        .pipe(plugins.htmlhint.failReporter())
+        .pipe(plugins.htmlmin({collapseWhitespace: true, removeComments: true}))
+        .pipe(plugins.ngHtml2js({
+            moduleName: "healthyGulpAngularApp"
+        }));
+};
+
+pipes.builtStylesDev = function() {
+    return gulp.src(paths.styles)
+        .pipe(gulp.dest(paths.distDev));
+};
+
+pipes.validatedIndex = function() {
+    return gulp.src(paths.index)
+        .pipe(plugins.htmlhint())
+        .pipe(plugins.htmlhint.reporter());
+};
+
+pipes.builtIndexDev = function() {
+
+    var orderedVendorScripts = pipes.builtVendorScriptsDev()
+        .pipe(pipes.orderedVendorScripts());
+
+    var orderedAppScripts = pipes.builtAppScriptsDev()
+        .pipe(pipes.orderedAppScripts());
+
+    var appStyles = pipes.builtStylesDev();
+
+    return pipes.validatedIndex()
+        .pipe(gulp.dest(paths.distDev)) // write first to get relative path for inject
+        .pipe(plugins.inject(orderedVendorScripts, {relative: true, name: 'bower'}))
+        .pipe(plugins.inject(orderedAppScripts, {relative: true}))
+        .pipe(plugins.inject(appStyles, {relative: true}))
+        .pipe(gulp.dest(paths.distDev));
+};
+
+gulp.task('validate-partials', pipes.validatedPartials);
+
+gulp.task('validate-index', pipes.validatedIndex);
+
+gulp.task('build-index-dev', pipes.builtIndexDev);
 
 gulp.task("default", function() {
   console.log("Hello World");
@@ -49,7 +128,7 @@ gulp.task("jshint", function() {
 });
 
 gulp.task("clean", function() {
-    gulp.src("./dist/*")
+    gulp.src("./dist.dev")
       .pipe(clean({force: true}));
 });
 
